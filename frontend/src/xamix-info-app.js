dc87ffa58892078ae82cd0550daed29f3a18f1ce
tlenexkoyotl <tlenexkoyotl@gmail.com>
2020-11-18 13:52:15 -0600

    Removed foreign-page

--- frontend/src/xamix-info-app.js
+++ frontend/src/xamix-info-app.js
@@ -1,7 +1,7 @@
-import {LitElement, html} from 'lit-element';
+import { LitElement, html } from 'lit-element';
 
-import {styles} from './xamix-info-styles.js';
-import {fadeInTransitionPage} from './utils/xamix-info-transitions.js';
+import { styles } from './xamix-info-styles.js';
+import { fadeInTransitionPage } from './utils/xamix-info-transitions.js';
 
 import '@polymer/paper-button/paper-button.js';
 import 'xamix-element';
@@ -10,13 +10,12 @@ import './pages/home-page';
 import './pages/calligraphy-page';
 import './pages/punctuation-page';
 import './pages/example-page';
-import './pages/foreign-page';
 
 class XamixInfo extends LitElement {
     static get properties() {
         return {
-            appTitle: {type: String},
-            _page: {type: String}
+            appTitle: { type: String },
+            _page: { type: String },
         };
     }
 
@@ -38,71 +37,62 @@ class XamixInfo extends LitElement {
     render() {
         return html`
         <style>
-        .main-app.out {
-              animation: ${this._config.transition.type}TransitionPage ${this._config.transition.delay / 1000}s;
-          }
-          
-          main-role{
-            max-height: 80vh;
-          }
+            .main-app.out {
+                animation: ${this._config.transition.type}TransitionPage ${this._config.transition.delay / 1000}s;
+            }
+
+            main-role {
+                max-height: 80vh;
+            }
         </style>
         <header class="header-app">
-        <div class="title container-app">
-            <xamix-element class="title"
-                textInput="xa-mix-tla'-qi-lo-lis-tli">
-                <span slot="after" style="font-size: ${document.body.clientWidth > 600 ? '1.5' : '1'}vw">Xamixtla'qilolistli</span>  
-            </xamix-element>
-        </div>
-        <nav class="navbar-principal">
-          <paper-button @click="${this.changePage}" page="home">
-              <xamix-element class="title"
-                textInput="pe-wal-tli">
-                <span slot="after">inicio</span>  
-              </xamix-element>
-          </paper-button>
-          <paper-button @click="${this.changePage}" page="calligraphy">
-            <xamix-element class="title"
-                textInput="tla-tek-pa-na-lis-tli">
-                <span slot="after">caligrafía</span>  
-            </xamix-element>
-          </paper-button>
-          <paper-button @click="${this.changePage}" page="punctuation">
-            <xamix-element class="title"
-                textInput="tla'-tol-ma-ci-o-yotl">
-                <span slot="after">puntuación</span>  
-            </xamix-element>
-          </paper-button>
-          <paper-button @click="${this.changePage}" page="example">
-            <xamix-element class="title"
-                textInput="ma-ci-o-me'">
-                <span slot="after">ejemplos</span>  
-            </xamix-element>
-          </paper-button>
-          <paper-button @click="${this.changePage}" page="foreign">
-            <xamix-element class="title"
-                textInput="con-tal-tli">
-                <span slot="after">extranjero</span>  
-            </xamix-element>
-          </paper-button>
-        </nav>
+            <div class="title container-app">
+            <paper-button @click="${this.changePage}" page="home">
+                <xamix-element class="title" textInput="xa-mix-tla'-qi-lo-lis-tli">
+                <br slot="after" />
+                <span slot="after">xamixtla'kwilolistli</span>
+                </xamix-element>
+            </paper-button>
+            </div>
+            <nav class="navbar-principal">
+            <paper-button @click="${this.changePage}" page="calligraphy">
+                <xamix-element class="title" textInput="tla-tek-pa-na-lis-tli">
+                <br slot="after" />
+                <span slot="after">caligrafía</span>
+                </xamix-element>
+            </paper-button>
+            <paper-button @click="${this.changePage}" page="punctuation">
+                <xamix-element class="title" textInput="tla'-tol-ma-ci-o-yotl">
+                <br slot="after" />
+                <span slot="after">puntuación</span>
+                </xamix-element>
+            </paper-button>
+            <paper-button @click="${this.changePage}" page="example">
+                <xamix-element class="title" textInput="ma-ci-o-me'">
+                <br slot="after" />
+                <span slot="after">ejemplos</span>
+                </xamix-element>
+            </paper-button>
+            </nav>
         </header>
         <main role="main" class="container-app main-app">
-        ${this._page === 'home' ?
-            html`<home-page class="page" active></home-page>` :
-            ''}
-        ${this._page === 'calligraphy' ?
-            html`<calligraphy-page class="page" active></calligraphy-page>` :
-            ''}
-        ${this._page === 'punctuation' ?
-            html`<punctuation-page class="page" active></punctuation-page>` :
-            ''}
-        ${this._page === 'example' ?
-            html`<example-page class="page" active></example-page>` :
-            ''}
-        ${this._page === 'foreign' ?
-            html`<foreign-page class="page" active></foreign-page>` :
-            ''}
+            ${this._page === 'home' ?
+                html`<home-page class="page" active></home-page>` :
+                ''}
+            ${this._page === 'calligraphy' ?
+                html`<calligraphy-page class="page" active></calligraphy-page>` :
+                ''}
+            ${this._page === 'punctuation' ?
+                html`<punctuation-page class="page" active></punctuation-page>` :
+                ''}
+            ${this._page === 'example' ?
+                html`<example-page class="page" active></example-page>` :
+                ''}
         </main>
+        <div class="footer">
+            <div>Copyright © 2019-2020 Rashīd Tlenexkoyotl</div>
+            <div>Esta página está licenciada bajo una Creative Commons Attribution-NoDerivatives 4.0 International License</div>
+        </div>
         `;
     }
 
@@ -124,7 +114,8 @@ class XamixInfo extends LitElement {
 
     updated(changedProps) {
         const allXamixElements = this.shadowRoot.querySelectorAll('xamix-element');
-        const buttonXamixElements = this.shadowRoot.querySelectorAll('nav>paper-button>xamix-element');
+        const titleXamixElements = this.shadowRoot.querySelectorAll('xamix-element.title');
+        const titleLatinElements = this.shadowRoot.querySelectorAll('xamix-element.title>span');
 
         this.setElementsAttribute(allXamixElements,
             [
@@ -144,11 +135,19 @@ class XamixInfo extends LitElement {
                 }
             ]);
 
-        this.setElementsAttribute(buttonXamixElements,
+        this.setElementsAttribute(titleXamixElements,
             [
                 {
                     name: 'fontSize',
-                    value: document.body.clientWidth > 600 ? '2' : '1.8'
+                    value: document.body.clientWidth > 600 ? '3' : '2.5'
+                }
+            ]);
+
+        this.setElementsAttribute(titleLatinElements,
+            [
+                {
+                    name: 'style',
+                    value: `font-size: ${document.body.clientWidth > 600 ? '1.8' : '1.5'}vw;`
                 }
             ]);
 
