f9c8ed7e8976c072bdebe752e77dd83368ba4494
tlenexkoyotl <tlenexkoyotl@gmail.com>
2020-12-04 17:41:05 -0600

    Updated styles

--- frontend/src/xamix-info-styles.js
+++ frontend/src/xamix-info-styles.js
@@ -1,411 +1,421 @@
 import { css } from "lit-element";
 
 export const styles = css`
-    :host {
-        overflow-x: scroll;
-        display: grid;
-        min-height: 100vh;
-        grid-template-areas: "header header header" "main   main   main" "footer footer footer";
-        grid-template-rows: auto 1fr auto;
-        direction: rtl;
-        background-color: #eeeeee;
-        color: #000000;
-        --xamix-italic-color: #c8a6ff;
-        --xamix-link-color: #00aaff;
-        --xamix-link-unvisited-color: #00aaff;
-        --xamix-link-visited-color: #ae7dff;
-        --xamix-link-focused-color: #ae7dff;
-        --xamix-link-hover-color: #ff0000;
-        --xamix-link-active-color: #ffff00;
-    }
+  :host {
+    overflow-x: scroll;
+    display: grid;
+    min-height: 100vh;
+    grid-template-areas: "header header header" "main   main   main" "footer footer footer";
+    grid-template-rows: auto 1fr auto;
+    direction: rtl;
+    background-color: #eeeeee;
+    color: #000000;
+    --xamix-italic-color: #c8a6ff;
+    --xamix-link-color: #00aaff;
+    --xamix-link-unvisited-color: #00aaff;
+    --xamix-link-visited-color: #ae7dff;
+    --xamix-link-focused-color: #ae7dff;
+    --xamix-link-hover-color: #ff0000;
+    --xamix-link-active-color: #ffff00;
+  }
 
+  .header-app {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    grid-area: header;
+    background-color: #794bc4;
+    color: #ffffff;
+    max-width: 98.9vw;
+  }
+
+  .main-app {
+    grid-area: main;
+  }
+
+  .footer {
+    width: 98.9vw;
+    padding-top: 3vw;
+    padding-bottom: 3vw;
+    text-align: center;
+    vertical-align: center;
+    background-color: #39245c;
+    color: #ffffff;
+  }
+
+  .footer > div {
+    font-size: 1.5vw;
+    text-align: center;
+    vertical-align: center;
+    color: #ffffff;
+  }
+
+  i {
+    --xamix-italic-color: #c8a6ff;
+  }
+
+  li {
+    display: block;
+    vertical-align: text-top;
+    padding-bottom: 2vw;
+  }
+
+  a {
+    color: #00aaff;
+  }
+
+  a:link {
+    color: #00aaff;
+  }
+
+  a:visited {
+    color: #ae7dff;
+  }
+
+  a:focus {
+    color: #ae7dff;
+  }
+
+  a:hover {
+    color: #ff0000;
+  }
+
+  a:active {
+    color: #ffff00;
+  }
+
+  .footer-app {
+    grid-area: footer;
+    height: 6vh;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .container-app {
+    padding: 2vw;
+    box-sizing: border-box;
+    background-color: inherit;
+  }
+
+  .container-footer {
+    padding: 2vw;
+    box-sizing: border-box;
+  }
+
+  .title {
+    padding: 0;
+    padding-left: 2vw;
+    padding-right: 2vw;
+    font-weight: lighter;
+    font-size: --latin-font-size;
+    flex: 1 auto;
+  }
+
+  .navbar-principal {
+    display: flex;
+    flex-flow: row nowrap;
+    line-height: 3vh;
+  }
+
+  .navbar-principal a {
+    padding: 0 2vw;
+    position: relative;
+  }
+
+  .latin {
+    font-size: var(--latin-font-size, 1.5vw);
+    direction: ltr;
+  }
+
+  xamix-element > h2.latin {
+    font-size: 2vw;
+  }
+
+  .latin > div > h2,
+  p {
+    margin: 0;
+  }
+
+  i {
+    color: #c8a6ff;
+  }
+
+  @media (max-width: 1280px) {
     .header-app {
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: space-between;
-        grid-area: header;
-        background-color: #794bc4;
-        color: #ffffff;
+      flex-flow: column nowrap;
     }
-
-    .main-app {
-        grid-area: main;
-    }
-
-    .footer {
-        width: 100vw;
-        padding-top: 3vw;
-        padding-bottom: 3vw;
-        text-align: center;
-        vertical-align: center;
-        background-color: #39245c;
-        color: #ffffff;
-    }
-
-    .footer > div {
-        font-size: 1.5vw;
-        text-align: center;
-        vertical-align: center;
-        color: #ffffff;
-    }
-
-    i {
-        --xamix-italic-color: #c8a6ff;
-    }
-
-    li {
-        display: block;
-        vertical-align: text-top;
-        padding-bottom: 2vw;
-    }
-
-    a {
-        color: #00aaff;
-    }
-
-    a:link {
-        color: #00aaff;
-    }
-
-    a:visited {
-        color: #ae7dff;
-    }
-
-    a:focus {
-        color: #ae7dff;
-    }
-
-    a:hover {
-        color: #ff0000;
-    }
-
-    a:active {
-        color: #ffff00;
-    }
-
-    .footer-app {
-        grid-area: footer;
-        height: 6vh;
-        display: flex;
-        justify-content: flex-start;
-    }
-
-    .container-app {
-        padding: 2vw;
-        box-sizing: border-box;
-        background-color: inherit;
-    }
-
-    .container-footer {
-        padding: 2vw;
-        box-sizing: border-box;
-    }
-
-    .title {
-        padding: 0;
-        padding-left: 2vw;
-        padding-right: 2vw;
-        font-weight: lighter;
-        font-size: --latin-font-size;
-        flex: 1 auto;
-    }
-
     .navbar-principal {
-        display: flex;
-        flex-flow: row nowrap;
-        line-height: 3vh;
+      flex: 1 auto;
+      justify-content: center;
     }
-
     .navbar-principal a {
-        padding: 0 2vw;
-        position: relative;
+      flex: 1 auto;
     }
+  }
 
-    .latin {
-        font-size: var(--latin-font-size, 1.5vw);
-        direction: ltr;
-    }
+  div.columns > div:not(.center) {
+    display: table-cell;
+  }
 
-    xamix-element > h2.latin {
-        font-size: 2vw;
-    }
+  div.tri-set > div:not(.center) {
+    display: table-cell;
+    max-width: 50vw;
+  }
 
-    .latin > div > h2,
-    p {
-        margin: 0;
-    }
+  div.tri-set > div:first-child {
+    padding-left: 1.5vw;
+  }
 
-    i {
-        color: #c8a6ff;
-    }
+  .columns > * {
+    display: table-cell;
+    padding-left: 1.5vw;
+    width: 33vw;
+  }
 
-    @media (max-width: 1280px) {
-        .header-app {
-        flex-flow: column nowrap;
-        }
-        .navbar-principal {
-        flex: 1 auto;
-        justify-content: center;
-        }
-        .navbar-principal a {
-        flex: 1 auto;
-        }
-    }
+  xamix-element {
+    font-size: 1.5vw;
+  }
 
-    div.columns > div:not(.center) {
-        display: table-cell;
-    }
+  xamix-element.title > h2 {
+    display: inline-block;
+  }
 
-    div.tri-set > div:not(.center) {
-        display: table-cell;
-        max-width: 50vw;
-    }
+  xamix-element > div {
+    display: table-cell;
+  }
 
-    div.tri-set > div:first-child {
-        padding-left: 1.5vw;
-    }
+  xamix-element > span {
+    font-size: 1vw;
+    vertical-align: top;
+  }
 
-    .columns > * {
-        display: table-cell;
-        padding-left: 1.5vw;
-        width: 33vw;
-    }
+  xamix-element > ::slotted(*) {
+    text-align: center;
+  }
 
-    xamix-element {
-        font-size: 1.5vw;
-    }
+  .top > * {
+    vertical-align: top;
+  }
 
-    xamix-element.title > h2 {
-        display: inline-block;
-    }
+  .center {
+    display: block;
+    align-self: center;
+  }
 
-    xamix-element > div {
-        display: table-cell;
-    }
+  .header {
+    vertical-align: top;
+    text-align: center;
+  }
 
-    xamix-element > span {
-        font-size: 1vw;
-        vertical-align: top;
-    }
+  div.table {
+    display: table;
+  }
 
-    xamix-element > ::slotted(*) {
-        text-align: center;
-    }
+  div.row {
+    display: table-row;
+    text-align: justify;
+    padding-bottom: 2vw;
+  }
 
-    .top > * {
-        vertical-align: top;
-    }
+  div.cell {
+    display: table-cell;
+    text-align: center;
+    padding-left: 3vw;
+  }
 
-    .center {
-        display: block;
-        align-self: center;
-    }
+  div.cell > svg {
+    max-width: 10vw;
+    fill: transparent;
+  }
 
-    .header {
-        vertical-align: top;
-        text-align: center;
-    }
+  div.cell > p:nth-of-type(1) > svg {
+    width: 13vw;
+  }
 
-    div.table {
-        display: table;
-    }
+  div.cell > p:nth-of-type(2) > svg {
+    width: 10vw;
+    fill: transparent;
+  }
 
-    div.row {
-        display: table-row;
-        text-align: justify;
-        padding-bottom: 2vw;
-    }
+  div.cell > xamix-element:not[.construction] > span {
+    font-size: 1vw;
+    vertical-align: top;
+  }
 
-    div.cell {
-        display: table-cell;
-        text-align: center;
-        padding-left: 3vw;
-    }
+  div.body.horizontal > xamix-element {
+    padding-bottom: 2vw;
+  }
 
-    div.cell > svg {
-        max-width: 10vw;
-        fill: transparent;
-    }
+  div.columns.header {
+    max-width: 10vw;
+  }
 
-    div.cell > p:nth-of-type(1) > svg {
-        width: 13vw;
-    }
+  div.columns.top.body {
+    max-width: 10vw;
+  }
 
-    div.cell > p:nth-of-type(2) > svg {
-        width: 10vw;
-        fill: transparent;
-    }
+  div.two-columns > div {
+    min-width: 50vw;
+  }
 
-    div.cell > xamix-element:not[.construction] > span {
-        font-size: 1vw;
-        vertical-align: top;
-    }
+  .justify {
+    text-align: justify;
+  }
 
-    div.body.horizontal > xamix-element {
-        padding-bottom: 2vw;
-    }
+  svg:not(.space) > * {
+    stroke: #000000;
+  }
 
-    div.columns.header {
-        max-width: 10vw;
-    }
+  svg:not(.space) > mask > * {
+    stroke: #ffffff;
+    fill: transparent;
+  }
 
-    div.columns.top.body {
-        max-width: 10vw;
-    }
+  svg:not(.space) > .directions {
+    fill: #000000;
+  }
 
-    div.two-columns > div {
-        min-width: 50vw;
-    }
+  svg.space > * {
+    stroke: transparent;
+  }
 
-    .justify {
-        text-align: justify;
-    }
+  .examples {
+    text-align: center;
+  }
 
-    svg:not(.space) > * {
-        stroke: #000000;
-    }
+  .examples > div > div > xamix-element > span {
+    font-size: 4vw;
+  }
 
-    svg:not(.space) > mask > * {
-        stroke: #ffffff;
-        fill: transparent;
-    }
+  span.colored-xamix > svg > * {
+    stroke: transparent;
+  }
 
-    svg:not(.space) > .directions {
-        fill: #000000;
-    }
+  span.colored-xamix > span {
+    vertical-align: top;
+  }
 
-    svg.space > * {
-        stroke: transparent;
-    }
+  span.colored-xamix > span.latin {
+    font-size: 4vw;
+  }
 
-    .examples {
-        text-align: center;
-    }
+  #phoneme-chart1 > div.row > div.cell > xamix-element > span {
+    font-size: 1.6vw;
+  }
 
-    .examples > div > div > xamix-element > span {
-        font-size: 4vw;
-    }
+  #punctuation-chart1 > div.row > div.cell > xamix-element > span {
+    font-size: 1.5vw;
+  }
 
-    span.colored-xamix > svg > * {
-        stroke: transparent;
-    }
+  #example1 {
+    padding-bottom: 2vw;
+  }
 
-    span.colored-xamix > span {
-        vertical-align: top;
-    }
+  table.construction {
+    margin-bottom: 3vw;
+    margin-right: 8vw;
+    max-width: 80vw;
+  }
 
-    span.colored-xamix > span.latin {
-        font-size: 4vw;
-    }
+  table.construction,
+  table.construction > tbody > tr > td,
+  table.construction > tbody > tr > th,
+  table.letters,
+  table.letters > tbody > tr > td,
+  table.letters > tbody > tr > th,
+  table.punctuation,
+  table.punctuation > tbody > tr > td,
+  table.punctuation > tbody > tr > th {
+    padding: 1vw;
+    border: 3px solid #000;
+    border-collapse: collapse;
+    text-align: center;
+    vertical-align: bottom;
+  }
 
-    #phoneme-chart1 > div.row > div.cell > xamix-element > span {
-        font-size: 1.6vw;
-    }
+  table.construction > tbody > tr > th > xamix-element > span,
+  table.letters > tbody > tr > th > xamix-element > span {
+    font-size: 1.5vw;
+    font-weight: normal;
+  }
 
-    #punctuation-chart1 > div.row > div.cell > xamix-element > span {
-        font-size: 1.5vw;
-    }
+  table.letters > tbody > tr > td > span {
+    font-size: 2vw;
+    font-weight: normal;
+  }
 
-    #example1 {
-        padding-bottom: 2vw;
-    }
+  table.construction > tbody > tr > td,
+  table.letters > tbody > tr > td {
+    direction: ltr;
+  }
 
-    table.construction {
-        margin-bottom: 3vw;
-        margin-right: 8vw;
-        max-width: 80vw;
-    }
+  img {
+    width: 100%;
+  }
 
-    table.construction,
-    table.construction > tbody > tr > td,
-    table.construction > tbody > tr > th,
-    table.letters,
-    table.letters > tbody > tr > td,
-    table.letters > tbody > tr > th {
-        padding: 1vw;
-        border: 3px solid black;
-        border-collapse: collapse;
-        text-align: center;
-        vertical-align: bottom;
-    }
+  .construction {
+    font-size: 2vw;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: black;
+  }
 
-    table.construction > tbody > tr > th > xamix-element > span,
-    table.letters > tbody > tr > th > xamix-element > span {
-        font-size: 1.5vw;
-        font-weight: normal;
-    }
+  li > * > * > .construction {
+    font-size: 3vw;
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: black;
+  }
 
-    table.letters > tbody > tr > td > span {
-        font-size: 2vw;
-        font-weight: normal;
-    }
+  .construction.v {
+    color: #09a38b;
+    -webkit-text-fill-color: #09a38b;
+  }
 
-    table.construction > tbody > tr > td,
-    table.letters > tbody > tr > td {
-        direction: ltr;
-    }
+  .construction.s {
+    color: #fdf6bd;
+    -webkit-text-fill-color: #fdf6bd;
+  }
 
-    img {
-        width: 100%;
-    }
+  .construction.a {
+    color: #217b9a;
+    -webkit-text-fill-color: #217b9a;
+  }
 
-    .construction {
-        font-size: 2vw;
-        -webkit-text-stroke-width: 1px;
-        -webkit-text-stroke-color: black;
-    }
+  .construction.k {
+    color: #ef4f3d;
+    -webkit-text-fill-color: #ef4f3d;
+  }
 
-    li > * > * > .construction {
-        font-size: 3vw;
-        -webkit-text-stroke-width: 1px;
-        -webkit-text-stroke-color: black;
-    }
+  .construction.b {
+    color: #ed45fa;
+    -webkit-text-fill-color: #ed45fa;
+  }
+  xamix-element.construction {
+    --xamix-text-stroke: #000;
+    --xamix-text-stroke-width: 3px;
+  }
 
-    .construction.v {
-        color: #09a38b;
-        -webkit-text-fill-color: #09a38b;
-    }
+  xamix-element.construction > * {
+    font-size: 2vw;
+  }
 
-    .construction.s {
-        color: #fdf6bd;
-        -webkit-text-fill-color: #fdf6bd;
-    }
+  .chart,
+  .chart > *,
+  .chart > * > *,
+  .chart > * > * > * {
+    font-size: 2vw;
+  }
 
-    .construction.a {
-        color: #217b9a;
-        -webkit-text-fill-color: #217b9a;
-    }
+  img#elements-svg {
+    width: 50%;
+  }
 
-    .construction.k {
-        color: #ef4f3d;
-        -webkit-text-fill-color: #ef4f3d;
-    }
+  div#elements-div {
+    text-align: center;
+  }
 
-    .construction.b {
-        color: #ed45fa;
-        -webkit-text-fill-color: #ed45fa;
-    }
-    xamix-element.construction {
-        --xamix-text-stroke: #000;
-        --xamix-text-stroke-width: 3px;
-    }
-
-    xamix-element.construction > * {
-        font-size: 2vw;
-    }
-
-    .chart,
-    .chart > *,
-    .chart > * > *,
-    .chart > * > * > * {
-        font-size: 2vw;
-    }
-
-    img#elements-svg {
-        width: 50%;
-    }
-
-    div#elements-div {
-        text-align: center;
-    }
+  paper-card#test,
+  paper-card#test-example {
+    margin: 1vw;
+    padding: 1vw;
+  }
 `;
